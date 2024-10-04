e964fe73c7fdf72cab31f66e1286582e7f5163c7
somelijer <116906162+somelijer@users.noreply.github.com>
2024-10-04 15:30:23 +0200

    fix: underscore bug

--- src/histogram.py
+++ src/histogram.py
@@ -3,6 +3,7 @@ import imageio.v3 as iio
 import skimage.color 
 import numpy as np
 import sys
+import cv2
 
 def HistogramExtractorFactory(type:str, histogram_bins:int = 256):
     if type == "RGB":
@@ -93,3 +94,24 @@ class RGBHistogramExtractor(HistogramExtractor):
         return self.histograms
     
 
+class HSVHistogramExtractor(HistogramExtractor):
+    def __init__(self, histogram_bins:int = 256):
+        super(GrayHistogramExtractor, self).__init__(histogram_bins)
+
+    def extract(self, image_path:str, normalize = True):
+        # Caution: image from imageio is RGB, from cv2 is BGR
+        image = iio.imread(image_path)
+        image = cv2.cvtcolor()
+        self.histograms = [] 
+        # RGB mode
+        bin_edges = np.linspace(0, 255, num=self.histogram_bins + 1)
+        for channel in range(image.shape[2]):
+            single_channel_img = image[:,:,channel]
+            channel_histogram, bin_edges = np.histogram(
+                single_channel_img.flatten(),
+                bins=bin_edges
+                )
+            channel_histogram = channel_histogram / channel_histogram.sum() if normalize else channel_histogram
+            self.histograms.append(channel_histogram)
+        
+        return self.histograms
