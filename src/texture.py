11b862985ade3ee8a54c4a607bebfe4ea7a68444
somelijer <116906162+somelijer@users.noreply.github.com>
2024-10-24 21:16:34 +0200

    feat: simple wavelet, needs more work

--- src/texture.py
+++ src/texture.py
@@ -1,5 +1,6 @@
 import os
 import imageio.v3 as iio
+import pywt
 import skimage.color 
 import numpy as np
 import sys
@@ -53,9 +54,10 @@ class LBPExtractor(TextureExtractor):
         # plt.imshow(lbp, cmap='gray')
         # plt.show()
         bin_edges = np.linspace(0, 255, num=self.histogram_bins + 1)
-        hist, _ = np.histogram(lbp.flatten(),bins=bin_edges)
+        histogram, _ = np.histogram(lbp.flatten(),bins=bin_edges)
+        histogram = histogram / histogram.sum() if normalize else histogram
 
-        return hist
+        return histogram
 
 class DCTExtractor(TextureExtractor):
     def __init__(self, histogram_bins:int = 256):
@@ -64,49 +66,6 @@ class DCTExtractor(TextureExtractor):
     def extract(self, image, normalize=True):
         return None
 
-class WaveletExtractor(TextureExtractor):
-    def __init__(self, histogram_bins:int = 256, wavelet='haar'):
-        super(WaveletExtractor, self).__init__(histogram_bins)
-        self.wavelet = wavelet
-
-    def extract(self, image, normalize=True):
-        return None
-
-class BlockHistogramExtractor(TextureExtractor):
-    def __init__(self, histogram_bins:int = 256, number_edge_block:int = 2):
-        self.number_edge_block = number_edge_block
-        super(BlockHistogramExtractor, self).__init__(histogram_bins)
-    
-    def extract(self, image, normalize = True):
-
-        sizei = image.shape[0]
-        sizej = image.shape[1]
-
-        sizei_block = int(sizei/self.number_edge_block)
-        sizej_block = int(sizej/self.number_edge_block)
-
-        image_vectors = []
-        for i in range(self.number_edge_block):
-            for j in range(self.number_edge_block):
-                i_left_bound = (sizei_block)*i
-                i_right_bound = (sizei_block*(i+1))
-                j_up_bound = (sizej_block)*j
-                j_down_bound = (sizej_block*(j+1))
-                # print("Square -----------------------")
-                # print(f"i bound [{i_left_bound},{i_right_bound}]")
-                # print(f"j bound[{j_up_bound},{j_down_bound}]")
-                # print("-----------------------")
-                block_image = image[i_left_bound:i_right_bound, j_up_bound:j_down_bound, :]
-                # imgplot = plt.imshow(block_image)
-                # plt.show()
-                image_vectors.append(block_image)
-        
-        hist = TextureExtractorFactory("LPB", self.histogram_bins)
-        hist_vector = [] #each histogram is a vector of histograms so we flaten them 
-        for subimage in image_vectors:
-            hist_vector = hist_vector + hist.extract(subimage, normalize)
-
-        return hist_vector
     
 class BlockLBPExtractor(TextureExtractor):
     def __init__(self, histogram_bins:int = 256, number_edge_block:int = 4):
@@ -150,14 +109,44 @@ class BlockLBPExtractor(TextureExtractor):
 
         return hist_vector
 
+#possible wavelets
+# haar, db2, sym3, bior1.3
+class WaveletExtractor(TextureExtractor):
+    def __init__(self, histogram_bins: int = 256, wavelet: str = 'haar', levels: int = 3):
+        super(WaveletExtractor, self).__init__(histogram_bins)
+        self.wavelet = wavelet  
+        self.levels = levels  # Number of decomposition levels
+
+    def extract(self, image, normalize=True):
+        # Convert the image to grayscale if it's not already
+        if len(image.shape) == 3:
+            gray_image = skimage.color.rgb2gray(image)
+        else:
+            gray_image = image
+
+        coeffs = pywt.wavedec2(gray_image, self.wavelet, level=self.levels)
+
+        # Extract the detail coefficients (LH, HL, HH)
+        features = []
+        # Skip the first entry (LL band)
+        for i in range(1, len(coeffs)):
+            cH, cV, cD = coeffs[i]
+            for sub_band in [cH, cV, cD]:
+                histogram, _ = np.histogram(sub_band, bins=self.histogram_bins, range=(sub_band.min(), sub_band.max()))
+                histogram = histogram / histogram.sum() if normalize else histogram
+                features.extend(histogram)
+
+        return np.array(features)
+
+
+
 if __name__ == "__main__":
 
     image = iio.imread('target/BBDD/bbdd_00003.jpg')
 
 
     extractor = TextureExtractorFactory("LBP",64)
-    
-    # Extract features
+
     features = extractor.extract(image)
     print(image.shape)
     print(len(features))
